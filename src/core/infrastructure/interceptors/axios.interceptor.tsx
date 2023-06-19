993135af2d5a1eeb80ace677e880026432b1fdaf
Rodrigo Orozco <orozcorodri8@gmail.com>
2023-06-19 19:34:03 -0300

    changes in local variables

--- src/core/infrastructure/interceptors/axios.interceptor.tsx
+++ src/core/infrastructure/interceptors/axios.interceptor.tsx
@@ -3,9 +3,8 @@ import axios from "axios";
 // this interceptor add headers key to use the api without adding it anymore in all the fetching data.
 export const accesKeyAxiosInterceptor = () => {
   axios.interceptors.request.use(function (config) {
-    config.headers["X-RapidAPI-Key"] =
-      "f3292ebbdbmsh9c2bc69a66306a7p139469jsncbd71041fc17";
-    config.headers["X-RapidAPI-Host"] = "api-nba-v1.p.rapidapi.com";
+    config.headers["X-RapidAPI-Key"] = import.meta.env.VITE_API_KEY;
+    config.headers["X-RapidAPI-Host"] = import.meta.env.VITE_API_HOST;
     return config;
   });
 };
